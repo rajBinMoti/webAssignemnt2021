cb4cc4baaa29d52d6815c6f16bc9bccc45c5ebd2
rajBinMoti <raj20032003@gmail.com>
2021-11-27 22:40:49 +0500

    Some warning fixes in header.php and index.php file

--- header.php
+++ header.php
@@ -1,8 +1,7 @@
 <?php
-$user = $_SESSION['_user'];
-if(isset($user)) {
+if (isset($_SESSION['_user'])) {
+    $user = $_SESSION['_user'];
     $login_menu_item = "Hello " . $user["user_full_name"] . ' (<a href="logout.php">Logout</a>)';
-
 }
 
 ?>
@@ -11,9 +10,9 @@ if(isset($user)) {
         <ul style="column-count: 3; list-style: none">
             <li><a href="home.php">Home</a></li>
             <li><a href="about.php">About</a></li>
-            <?php if(isset($login_menu_item)):?>
-                <li><?=$login_menu_item?></li>
-            <?php endif;?>
+            <?php if (isset($login_menu_item)) : ?>
+                <li><?= $login_menu_item ?></li>
+            <?php endif; ?>
         </ul>
     </nav>
 </header>
