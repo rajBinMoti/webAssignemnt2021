2e64699714296a3e4cbc76a22dc23f2690e47f68
rajBinMoti <raj20032003@gmail.com>
2021-12-01 00:08:09 +0500

    Post edit

--- editpost.php
+++ editpost.php
@@ -7,6 +7,41 @@ if (isset($_SESSION['_user'])) {
 
 require_once './utilities/db.php';
 
+function update_post($post_id, $post_title, $post_body, $post_check)
+{
+    try {
+        $db = db_connect();
+        $statement = "
+        UPDATE blogpost SET post_title = :post_title, post_body = :post_body, post_public = :post_check WHERE post_id = :post_id;
+        ";
+
+        $statement = $db->prepare($statement);
+        $statement->bindParam(":post_id", $post_id);
+        $statement->bindParam(":post_title", $post_title);
+        $statement->bindParam(":post_body", $post_body);
+        $statement->bindParam(":post_check", $post_check);
+
+        $statement->execute();
+
+        return "UPDATED";
+    } catch (PDOException $e) {
+        return "ERROR";
+    }
+}
+
+if (isset($_POST['update-post'])) {
+    $post_id = $_GET['post-id'];
+    $post_title = $_POST['post-title'];
+    $post_body = $_POST['post-body'];
+    $post_check = $_POST['post-check'] == '1' ? 1 : 0;
+
+    $res = update_post($post_id, $post_title, $post_body, $post_check);
+    if ($res == "UPDATED") {
+        header('Location: post.php?id=' . $post_id);
+    }
+}
+
+
 if (isset($_GET['post-id'])) {
     $db = db_connect();
 
@@ -50,7 +85,7 @@ if (isset($_GET['post-id'])) {
             <input type="checkbox" class="form-check-input" name="post-check" <?php $post['post_public'] == '1' ? 'checked' : '' ?>>
             <label for="post-check" class="form-label">Publish</label>
         </div>
-        <button class="btn btn-primary" size='sm'>Post</button>
+        <button class="btn btn-primary" name="update-post" size='sm'>UPDATE</button>
     </form>
 </body>
 
