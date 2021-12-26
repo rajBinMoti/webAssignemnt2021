9b8461e1b6569b986f4192d995384e3628d89e43
rajBinMoti <raj20032003@gmail.com>
2021-12-27 03:12:49 +0500

    Updated README.md and About.php

---
+++ README.md
@@ -0,0 +1,67 @@
+# BlogHost and Todo Web Assignment
+
+## Implementations
+
+1.  Like and Dislike functions.
+    - On .../post.php page registered users can like and dislike the post.
+2.  Add New or Edit Posts.
+    - Registered user can post new articles and can edit as well.
+3.  Enable and disable public visibility.
+    - Users can control the visibility of their post.
+4.  Read Posts.
+    - Read count will be updated by +1 when unique user reads post first time only.
+5.  Comments.
+    - New comments on post.
+    - Comments read section on post page.
+6.  User Profile.
+    - Shows totall likes.
+    - Shows totall Comments.
+    - Shows totall Posts Count.
+7.  Registration.
+    - User can register with unique name only.
+8.  Todo Session based with following features
+    - Categorised Tasks
+    - Custom Prioritised Tasks
+    - Checkbox to mark completion.
+    - Delete Button to remove task.
+9.  **Converted README.md into Raw HTML** using [Web Tool (Link).](https://markdowntohtml.com/)
+
+## How to setup and run the code
+
+Clone this repo using git clone https://github.com/rajBinMoti/webAssignemnt2021 in htdoc. There is a file dbSetup.php in root directory, which will generate a dummy database with some record to test the blog side of application. Run it on localhost using http://localhost/blog-host/install.php url.
+
+Note! To safe remove the dummy database: http://localhost/blog-host/uninstall.php
+
+## How to run the application
+
+Open browser and visit http://localhost/blog-host and test it on local server.
+One copy of this code is also deployed on live server at https://2k18-csm-84.000webhostapp.com/
+
+## Odds
+
+- Some logical fixes are still pending to do...will fix soon.
+
+## Glimpse
+
+### Login Steps
+
+![Login-Home-Logout](images\Inkedr2.gif)
+
+### Read, Like, and, Comment on Post
+
+![Read-Like-Comment](images\Inkedr1.gif)
+
+### Add Task
+
+![AddTask](images\AddTask.gif)
+
+### Update Task
+
+![UpdateTask](images\UpdateTask.gif)
+
+### Delete Task
+
+![DeleteTask](images\DeleteTask.gif)
+
+
+# Feedback me at 
--- about.php
+++ about.php
@@ -11,11 +11,84 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About</title>
+    <style>
+        img{
+            width: 80vw;
+        }
+    </style>
 </head>
 
 <body>
     <?php require_once './header.php' ?>
 
+    <div class="container">
+
+
+        <h1 id="bloghost-and-todo-web-assignment">BlogHost and Todo Web Assignment</h1>
+        <h2 id="implementations">Implementations</h2>
+        <ol>
+            <li>Like and Dislike functions.<ul>
+                    <li>On .../post.php page registered users can like and dislike the post.</li>
+                </ul>
+            </li>
+            <li>Add New or Edit Posts.<ul>
+                    <li>Registered user can post new articles and can edit as well.</li>
+                </ul>
+            </li>
+            <li>Enable and disable public visibility.<ul>
+                    <li>Users can control the visibility of their post.</li>
+                </ul>
+            </li>
+            <li>Read Posts.<ul>
+                    <li>Read count will be updated by +1 when unique user reads post first time only.</li>
+                </ul>
+            </li>
+            <li>Comments.<ul>
+                    <li>New comments on post.</li>
+                    <li>Comments read section on post page.</li>
+                </ul>
+            </li>
+            <li>User Profile.<ul>
+                    <li>Shows totall likes.</li>
+                    <li>Shows totall Comments.</li>
+                    <li>Shows totall Posts Count.</li>
+                </ul>
+            </li>
+            <li>Registration.<ul>
+                    <li>User can register with unique name only.</li>
+                </ul>
+            </li>
+            <li>Todo Session based with following features<ul>
+                    <li>Categorised Tasks</li>
+                    <li>Custom Prioritised Tasks</li>
+                    <li>Checkbox to mark completion.</li>
+                    <li>Delete Button to remove task.</li>
+                </ul>
+            </li>
+        </ol>
+        <h2 id="how-to-setup-and-run-the-code">How to setup and run the code</h2>
+        <p>Clone this repo using git clone <a href="https://github.com/rajBinMoti/webAssignemnt2021">https://github.com/rajBinMoti/webAssignemnt2021</a> in htdoc. There is a file dbSetup.php in root directory, which will generate a dummy database with some record to test the blog side of application. Run it on localhost using <a href="http://localhost/blog-host/install.php">http://localhost/blog-host/install.php</a> url.</p>
+        <p>Note! To safe remove the dummy database: <a href="http://localhost/blog-host/uninstall.php">http://localhost/blog-host/uninstall.php</a></p>
+        <h2 id="how-to-run-the-application">How to run the application</h2>
+        <p>Open browser and visit <a href="http://localhost/blog-host">http://localhost/blog-host</a> and test it on local server.
+            One copy of this code is also deployed on live server at <a href="https://2k18-csm-84.000webhostapp.com/">https://2k18-csm-84.000webhostapp.com/</a></p>
+        <h2 id="odds">Odds</h2>
+        <ul>
+            <li>Some logical fixes are still pending to do...will fix soon.</li>
+        </ul>
+        <h2 id="glimpse">Glimpse</h2>
+        <h3 id="login-steps">Login Steps</h3>
+        <p><img src="images\Inkedr2.gif" alt="Login-Home-Logout"></p>
+        <h3 id="read-like-and-comment-on-post">Read, Like, and, Comment on Post</h3>
+        <p><img src="images\Inkedr1.gif" alt="Read-Like-Comment"></p>
+        <h3 id="add-task">Add Task</h3>
+        <p><img src="images\AddTask.gif" alt="AddTask"></p>
+        <h3 id="update-task">Update Task</h3>
+        <p><img src="images\UpdateTask.gif" alt="UpdateTask"></p>
+        <h3 id="delete-task">Delete Task</h3>
+        <p><img src="images\DeleteTask.gif" alt="DeleteTask"></p>
+
+    </div>
 </body>
 
 </html>
